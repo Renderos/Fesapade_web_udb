2afc2b7fd97c02483afc3772110f934b74b5c339
Ronald Renderos <rnld.renderos24@gmail.com>
2026-04-23 10:38:35 -0600

    feat: enhance security middleware configuration and update subtitle in CursosPage

--- fesapade-strapi/config/middlewares.ts
+++ fesapade-strapi/config/middlewares.ts
@@ -1,9 +1,34 @@
 import type { Core } from '@strapi/strapi';
 
-export default ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => [
+export default ({ env }: { env: any }): any[] => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com', // Dominio de Cloudinary
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com', // Dominio de Cloudinary
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
